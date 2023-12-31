f1a12e1fcfc905696cdf20af509ea92f6b87f6a2
khoaihps <khoadoan2472003@gmail.com>
2023-12-31 08:46:24 +0700

    add data

--- cleaning/output/merge_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('./cleaning/output/batdongsan.tsv', sep='\t')
-df2 = pd.read_csv('./cleaning/output/nhatot.tsv', sep='\t')
-
-result_df = pd.concat([df1, df2], ignore_index=True)
-
-result_df.to_csv('./cleaning/output/merged_file.tsv', sep='\t', index=False)
